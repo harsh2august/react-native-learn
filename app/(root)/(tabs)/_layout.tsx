5b1f846d571257927a8e745880da76054485df17
Harsh Kumar <harshj7.net@gmail.com>
2026-08-26 16:11:14 +0530

    feat: add icon color customization to NativeTabs component

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -2,7 +2,7 @@ import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   return (
-    <NativeTabs>
+    <NativeTabs iconColor={"red"}>
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
         <Icon sf="house.fill" drawable="custom_android_drawable" />
